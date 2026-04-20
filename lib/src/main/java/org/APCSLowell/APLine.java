c591064a6305d9dd43f3ff1ef411f64fa6400dd7
breathaning <etzhu1@gmail.com>
2026-04-20 17:14:29 +0000

    submit

--- lib/src/main/java/org/APCSLowell/APLine.java
+++ lib/src/main/java/org/APCSLowell/APLine.java
@@ -1,12 +1,16 @@
 package org.APCSLowell;
 
 public class APLine {
-    int a, b, c;
-    public APLine(int a, int b, int c) {}
+    private int a, b, c;
+    public APLine(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
     public boolean isOnLine(int x, int y) {
         return a * x + b * y + c == 0;
     }
     public double getSlope() {
-        return -(double)a / (double)b;
+        return -((double)a / b);
     }
 }
